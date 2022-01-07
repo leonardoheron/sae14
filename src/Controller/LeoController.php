d262957d3e1565b5d7bad1907a5f148694739ad9
leonardoheron <96128400+leonardoheron@users.noreply.github.com>
2022-01-07 17:58:07 +0100

    upadte_add_pages

--- src/Controller/LeoController.php
+++ src/Controller/LeoController.php
@@ -10,11 +10,21 @@ class LeoController extends AbstractController
 {
     
     /**
-     * @Route("/", name="")
+     * @Route("/portfolio", name="portfolio")
      */
     public function portfolio(): Response
     {
         return $this->render('leo/portfolio.html.twig', [
+            'controller_name' => 'LeoController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="")
+     */
+    public function accueil(): Response
+    {
+        return $this->render('leo/accueil.html.twig', [
             'controller_name' => 'LeoController',
         ]);
     }
